532f21db093edd989f017fde2f7f9ac3d31ef667
Fernirx <anhtuhungdeveloper@gmail.com>
2025-08-03 00:28:48 +0700

    feat: Add main application module and config pom.xml for 'application' module

--- application/src/main/java/com/fernirx/lms/application/LmsApplication.java
+++ application/src/main/java/com/fernirx/lms/application/LmsApplication.java
@@ -1,4 +1,4 @@
-package com.fernirx.lms;
+package com.fernirx.lms.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- application/src/test/java/com/fernirx/lms/application/LmsApplicationTests.java
+++ application/src/test/java/com/fernirx/lms/application/LmsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.fernirx.lms;
+package com.fernirx.lms.application;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
