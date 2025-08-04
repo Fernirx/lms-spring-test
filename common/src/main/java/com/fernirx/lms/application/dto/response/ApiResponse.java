9fd0df2a1139b0988a2fd2101b6261f334661eb6
Fernirx <anhtuhungdeveloper@gmail.com>
2025-08-04 18:38:41 +0700

    feat(common): add standardized ApiResponse and ErrorResponse classes

---
+++ common/src/main/java/com/fernirx/lms/application/dto/response/ApiResponse.java
@@ -0,0 +1,73 @@
+package com.fernirx.lms.application.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ssXXX")
+    private ZonedDateTime timestamp;
+    private int status;
+    private Boolean success;
+    private String message;
+    private List<ErrorResponse> errors;
+    private T data;
+
+    public static <T> ApiResponse<T> success(T data) {
+        return ApiResponse.<T>builder()
+                .timestamp(ZonedDateTime.now())
+                .status(200)
+                .success(true)
+                .message("Success")
+                .data(data)
+                .build();
+    }
+
+    public static <T> ApiResponse<T> success(T data, String message) {
+        return ApiResponse.<T>builder()
+                .timestamp(ZonedDateTime.now())
+                .status(200)
+                .success(true)
+                .message(message)
+                .data(data)
+                .build();
+    }
+
+    public static <T> ApiResponse<T> success(String message) {
+        return ApiResponse.<T>builder()
+                .timestamp(ZonedDateTime.now())
+                .status(200)
+                .success(true)
+                .message(message)
+                .build();
+    }
+
+    public static <T> ApiResponse<T> success() {
+        return ApiResponse.<T>builder()
+                .timestamp(ZonedDateTime.now())
+                .status(200)
+                .success(true)
+                .message("Success")
+                .build();
+    }
+
+    public static <T> ApiResponse<T> error(int status, String message,  List<ErrorResponse> errors) {
+        return ApiResponse.<T>builder()
+                .timestamp(ZonedDateTime.now())
+                .status(status)
+                .success(false)
+                .message(message)
+                .errors(errors)
+                .build();
+    }
+}
---
+++ common/src/main/java/com/fernirx/lms/application/dto/response/ErrorResponse.java
@@ -0,0 +1,18 @@
+package com.fernirx.lms.application.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ErrorResponse {
+    private String code;
+    private String message;
+    private String field;
+}
