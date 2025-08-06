fec9591a4bb004e88eac9fe258fed6d7fa354d1d
Fernirx <anhtuhungdeveloper@gmail.com>
2025-08-06 17:31:35 +0700

    refactor(common): delete space in ErrorDetail.java

--- common/src/main/java/com/fernirx/lms/common/dtos/responses/ErrorDetail.java
+++ common/src/main/java/com/fernirx/lms/common/dtos/responses/ErrorDetail.java
@@ -1,43 +1,43 @@
-    package com.fernirx.lms.common.dtos.responses;
+package com.fernirx.lms.common.dtos.responses;
 
-    import com.fasterxml.jackson.annotation.JsonInclude;
-    import com.fernirx.lms.common.enums.ErrorCode;
-    import io.swagger.v3.oas.annotations.media.Schema;
-    import lombok.AllArgsConstructor;
-    import lombok.Builder;
-    import lombok.Data;
-    import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fernirx.lms.common.enums.ErrorCode;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-    @Data
-    @NoArgsConstructor
-    @AllArgsConstructor
-    @Builder(toBuilder = true)
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    @Schema(description = "Specific details of the error in the system")
-    public class ErrorDetail {
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Schema(description = "Specific details of the error in the system")
+public class ErrorDetail {
 
-        @Schema(description = "Error code", example = "ERR_VALIDATION")
-        private ErrorCode code;
+    @Schema(description = "Error code", example = "ERR_VALIDATION")
+    private ErrorCode code;
 
-        @Schema(description = "Error message", example = "Username is required")
-        private String message;
+    @Schema(description = "Error message", example = "Username is required")
+    private String message;
 
-        @Schema(description = "Field name (if applicable)", example = "username")
-        private String field;
+    @Schema(description = "Field name (if applicable)", example = "username")
+    private String field;
 
-        public static ErrorDetail of(ErrorCode errorCode, String field) {
-            return of(errorCode, errorCode.getMessage(), field);
-        }
-
-        public static ErrorDetail of(ErrorCode errorCode) {
-            return of(errorCode, null);
-        }
-
-        public static ErrorDetail of(ErrorCode errorCode, String customMessage, String field) {
-            return ErrorDetail.builder()
-                    .code(errorCode)
-                    .message(customMessage)
-                    .field(field)
-                    .build();
-        }
+    public static ErrorDetail of(ErrorCode errorCode, String field) {
+        return of(errorCode, errorCode.getMessage(), field);
     }
+
+    public static ErrorDetail of(ErrorCode errorCode) {
+        return of(errorCode, null);
+    }
+
+    public static ErrorDetail of(ErrorCode errorCode, String customMessage, String field) {
+        return ErrorDetail.builder()
+                .code(errorCode)
+                .message(customMessage)
+                .field(field)
+                .build();
+    }
+}
