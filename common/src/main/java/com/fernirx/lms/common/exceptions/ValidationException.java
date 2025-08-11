07e427bdcfd2c3d5dd8a7f528893b28b24ec6b75
Fernirx <anhtuhungdeveloper@gmail.com>
2025-08-11 22:11:31 +0700

    feat(common): add custom exception classes for LMS project

---
+++ common/src/main/java/com/fernirx/lms/common/exceptions/ValidationException.java
@@ -0,0 +1,15 @@
+package com.fernirx.lms.common.exceptions;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class ValidationException extends RuntimeException {
+    private final List<String> errors;
+
+    public ValidationException(List<String> errors) {
+        super("Validation failed");
+        this.errors = errors;
+    }
+}
