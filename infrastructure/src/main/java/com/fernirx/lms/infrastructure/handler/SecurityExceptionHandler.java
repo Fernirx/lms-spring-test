781367dba2e38432ea30ea8310e7184cb8100a7d
Fernirx <anhtuhungdeveloper@gmail.com>
2025-08-17 23:32:05 +0700

    feat: Add centralized security exception(JWT) handler

--- infrastructure/src/main/java/com/fernirx/lms/infrastructure/handler/SecurityExceptionHandler.java
+++ infrastructure/src/main/java/com/fernirx/lms/infrastructure/handler/SecurityExceptionHandler.java
@@ -3,7 +3,7 @@ package com.fernirx.lms.infrastructure.handler;
 import com.fernirx.lms.common.dtos.responses.ErrorDetail;
 import com.fernirx.lms.common.dtos.responses.ErrorResponse;
 import com.fernirx.lms.common.enums.ErrorCode;
-import com.fernirx.lms.common.exceptions.InvalidTokenTypeException;
+import com.fernirx.lms.common.exceptions.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,12 +13,49 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class SecurityExceptionHandler {
 
+    @ExceptionHandler(InvalidTokenException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidTokenException(InvalidTokenException ex) {
+        log.error("Invalid token (signature/illegal argument)", ex);
+        return buildErrorResponse(ex.getErrorCode());
+    }
+
+    @ExceptionHandler(JwtValidationException.class)
+    public ResponseEntity<ErrorResponse> handleJwtValidationException(JwtValidationException ex) {
+        log.error("Unexpected JWT validation error", ex);
+        return buildErrorResponse(ex.getErrorCode());
+    }
+
+    @ExceptionHandler(ExpiredTokenException.class)
+    public ResponseEntity<ErrorResponse> handleExpiredTokenException(ExpiredTokenException ex) {
+        log.warn("Token has expired", ex);
+        return buildErrorResponse(ex.getErrorCode());
+    }
+
     @ExceptionHandler(InvalidTokenTypeException.class)
     public ResponseEntity<ErrorResponse> handleInvalidTokenTypeException(InvalidTokenTypeException ex) {
-        log.error("Invalid token type. Expected {} but got {}", ex.getExpectedType(), ex.getTokenType());
-        ErrorCode errorCode = ex.getErrorCode();
+        log.error("Invalid token type. Expected {} but got {}", ex.getExpectedType(), ex.getTokenType(), ex);
+        return buildErrorResponse(ex.getErrorCode());
+    }
+
+    @ExceptionHandler(MalformedTokenException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidMalformedTokenException(MalformedTokenException ex) {
+        log.error("Malformed token", ex);
+        return buildErrorResponse(ex.getErrorCode());
+    }
+
+    @ExceptionHandler(UnsupportedTokenException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidUnsupportedTokenException(UnsupportedTokenException ex) {
+        log.error("Unsupported token", ex);
+        return buildErrorResponse(ex.getErrorCode());
+    }
+
+    public ResponseEntity<ErrorResponse> buildErrorResponse(ErrorCode errorCode) {
         ErrorDetail errorDetail = ErrorDetail.of(errorCode);
-        ErrorResponse errorResponse = ErrorResponse.of(errorCode.getCategory(), errorCode.getMessage(), errorDetail);
+        ErrorResponse errorResponse = ErrorResponse.of(
+                errorCode.getCategory(),
+                errorCode.getMessage(),
+                errorDetail
+        );
         return ResponseEntity
                 .status(errorCode.getHttpStatus())
                 .body(errorResponse);
