696d6e505454493b23ba8be98ac3f1d8c236a466
Fernirx <anhtuhungdeveloper@gmail.com>
2025-08-17 23:31:20 +0700

    feat(common): Add custom JWT exception classes

---
+++ common/src/main/java/com/fernirx/lms/common/exceptions/ExpiredTokenException.java
@@ -0,0 +1,11 @@
+package com.fernirx.lms.common.exceptions;
+
+import com.fernirx.lms.common.enums.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class ExpiredTokenException extends LmsException {
+    public ExpiredTokenException () {
+        super(ErrorCode.EXPIRED_TOKEN);
+    }
+}
---
+++ common/src/main/java/com/fernirx/lms/common/exceptions/MalformedTokenException.java
@@ -0,0 +1,11 @@
+package com.fernirx.lms.common.exceptions;
+
+import com.fernirx.lms.common.enums.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class MalformedTokenException extends LmsException {
+    public MalformedTokenException() {
+        super(ErrorCode.INVALID_TOKEN_FORMAT);
+    }
+}
