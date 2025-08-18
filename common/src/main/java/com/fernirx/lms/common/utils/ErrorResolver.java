3197235321659e3319ab2fce11fe9ce860dc63c5
Fernirx <anhtuhungdeveloper@gmail.com>
2025-08-18 18:11:22 +0700

    refactor: rename ErrorCategoryResolver -> ErrorResolver

---
+++ common/src/main/java/com/fernirx/lms/common/utils/ErrorResolver.java
@@ -0,0 +1,28 @@
+package com.fernirx.lms.common.utils;
+
+import com.fernirx.lms.common.dtos.responses.ErrorDetail;
+import com.fernirx.lms.common.dtos.responses.ErrorResponse;
+import com.fernirx.lms.common.enums.ErrorCategory;
+import com.fernirx.lms.common.enums.ErrorCode;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+public final class ErrorResolver {
+
+    private ErrorResolver() {
+        throw new UnsupportedOperationException("Utility class ");
+    }
+
+    public static Optional<ErrorCode> getHighestPriorityErrorCode(List<ErrorDetail> details) {
+        return details.stream()
+                .map(ErrorResolver::mapDetailToErrorCode)
+                .flatMap(Optional::stream)
+                .max(Comparator.comparingInt(ErrorCode::getLevel));
+    }
+
+    private static Optional<ErrorCode> mapDetailToErrorCode(ErrorDetail detail) {
+        return Optional.ofNullable(detail.getCode());
+    }
+}
