641182266aab4363ed4cdffb27ea36877d1629ea
Fernirx <anhtuhungdeveloper@gmail.com>
2025-08-25 23:56:59 +0700

    feat: add HTTP header constants to ApiConstants
    - Add AUTHORIZATION_HEADER, BEARER_PREFIX constants
    - Add BEARER_PREFIX_LENGTH for consistent token parsing
    - Centralize HTTP-related constants for reusability

--- common/src/main/java/com/fernirx/lms/common/constants/ApiConstants.java
+++ common/src/main/java/com/fernirx/lms/common/constants/ApiConstants.java
@@ -20,7 +20,8 @@ public final class ApiConstants {
 
     // ========== HTTP HEADERS ==========
     public static final String AUTHORIZATION_HEADER = "Authorization";
-    public static final String BEARER_PREFIX = "Bearer";
+    public static final String BEARER_PREFIX = "Bearer ";
+    public static final int BEARER_PREFIX_LENGTH = 7;
     public static final String CONTENT_TYPE_JSON = "application/json";
 
     private ApiConstants() {
